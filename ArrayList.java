ad88a45ad9e1a066f6e73b4d3e9573b4b1ef1224
prathameshgotur <85452112+prathameshgotur@users.noreply.github.com>
2021-06-08 19:22:26 +0530

    Add files via upload

--- ArrayList.java
+++ ArrayList.java
@@ -1,68 +1,40 @@
 package Collection;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
 
-public class altest {
-
+public class altest1 {
+	
 	public static void main(String[] args) {
 		
-		ArrayList<String> fruits=new ArrayList<String>();
+		// Conversion of ArrayList to Arry 
+		ArrayList<String> alist=new ArrayList<String>();
+		alist.add("A");
+		alist.add("B");
+		alist.add("C");
+		alist.add("D");
+		alist.add("E");
+		System.out.println(alist);
 		
-		fruits.add("Apple");
-		fruits.add("Mango");
-		fruits.add("Banana");
-		fruits.add("Watermelon");
-		ArrayList<Integer> qty=new ArrayList<Integer>();
-		qty.add(10);
-		qty.add(20);
-		qty.add(30);
-		qty.add(40);
+		String[] arr=new String[alist.size()];
 		
-		//we have to uses set()
-
-		//qty.set(1, 50);
+		alist.toArray(arr);
+		System.out.println("Array:");
 		
-		//remove element
-		
-		qty.remove(1);
-		
-		fruits.remove("Watermelon");
-
-		for(String s:fruits)
+		for(String s:arr)
 			System.out.println(s);
+		System.out.println("Array length:"+arr.length);
 		
-		
-		System.out.println(qty);
-		//Size()
-		System.out.println("Fruits Size: "+ fruits.size());
-		System.out.println("Qty Size: "+ qty.size());
-		//get()
-		
-		System.out.println(fruits.get(1));
-		System.out.println(qty.get(0));
-		fruits.clear();
-		System.out.println("Fruits Size Ater "+fruits.size());
-		System.out.println(qty.isEmpty());
-		System.out.println(fruits.isEmpty());
-		
-		//how to copy all elements to another arry
-		
-		ArrayList<Integer> qty1=new ArrayList<Integer>();
-		qty1.addAll(qty);
-		
-		qty1.add(80);
-		qty1.add(81);
-		qty1.add(82);
-		
-		System.out.println(qty1);
-		
-		//reverse()
-		Collections.reverse(qty1);
-		System.out.println(qty1);
-		
-		
-	}
+	}}//class ends
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
